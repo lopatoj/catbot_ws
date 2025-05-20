d1eec9ef8c5982fe2d3d908d729b822e8336814f
Justin Lopato <justin@lopato.org>
2025-05-20 01:27:02 -0500

    got it working yay

--- src/catbot_launch/setup.py
+++ src/catbot_launch/setup.py
@@ -1,25 +1,33 @@
+import os
+
+from glob import glob
 from setuptools import find_packages, setup
 
-package_name = 'catbot_launch'
+package_name = "catbot_launch"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        (
+            "share/ament_index/resource_index/packages",
+            ["resource/" + package_name],
+        ),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            "share/" + package_name + "/launch",
+            glob(os.path.join("launch", "*.launch.[py][ya]*")),
+        ),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='lopatoj',
-    maintainer_email='justin@lopato.org',
-    description='TODO: Package description',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer="lopatoj",
+    maintainer_email="justin@lopato.org",
+    description="TODO: Package description",
+    license="MIT",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
--- src/catbot_pybullet/catbot_pybullet/pybullet_node.py
+++ src/catbot_pybullet/catbot_pybullet/pybullet_node.py
@@ -2,7 +2,7 @@ import rclpy
 import pybullet as p
 import pybullet_data
 
-from rclpy import Node
+from rclpy.node import Node
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Float64MultiArray
 
@@ -10,7 +10,9 @@ from std_msgs.msg import Float64MultiArray
 class PyBulletNode(Node):
     def __init__(self):
         super().__init__("pybullet_node")
-        
+
+        self.declare_parameter("dt", 0.01)
+
         self.client = p.connect(p.GUI)
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         p.setGravity(0, 0, -10)
@@ -18,22 +20,53 @@ class PyBulletNode(Node):
         self.plane_id = p.loadURDF("plane.urdf")
         self.robot_id = p.loadURDF(
             "install/catbot_pybullet/share/catbot_pybullet/description/robot.urdf",
-            [0, 0, 0],
+            [0, 0, 0.5],
             p.getQuaternionFromEuler([0, 0, 0]),
             flags=p.URDF_USE_INERTIA_FROM_FILE | p.URDF_MAINTAIN_LINK_ORDER,
         )
         self.num_joints = p.getNumJoints(bodyUniqueId=self.robot_id)
+        self.motor_indices = []
 
         self.joint_states = JointState()
+        self.joint_name_to_index = {}
 
         for i in range(self.num_joints):
             joint_info = p.getJointInfo(
                 bodyUniqueId=self.robot_id, jointIndex=i
             )
-            self.joint_states.name.append(str(joint_info[1]))
-            self.joint_states.position.append(0.0)
-            self.joint_states.velocity.append(0.0)
-            self.joint_states.effort.append(0.0)
+            name: str = joint_info[1].decode("utf-8")
+            self.joint_name_to_index[name] = i
+
+            if "motor" in name:
+                self.motor_indices.append(i)
+                self.joint_states.name.append(name)
+                self.joint_states.position.append(0.0)
+                self.joint_states.velocity.append(0.0)
+                self.joint_states.effort.append(0.0)
+
+        self.get_logger().info(f"{self.joint_name_to_index}")
+
+        p.createConstraint(
+            self.robot_id,
+            self.joint_name_to_index["closing_passive5_1_frame"],
+            self.robot_id,
+            self.joint_name_to_index["closing_passive5_2_frame"],
+            p.JOINT_POINT2POINT,
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        )
+
+        p.createConstraint(
+            self.robot_id,
+            self.joint_name_to_index["closing_passive3_1_frame"],
+            self.robot_id,
+            self.joint_name_to_index["closing_passive3_2_frame"],
+            p.JOINT_POINT2POINT,
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        )
 
         p.setJointMotorControlArray(
             bodyIndex=self.robot_id,
@@ -50,20 +83,20 @@ class PyBulletNode(Node):
         )
 
         self.create_timer(self.get_parameter("dt").value, self._step_callback)
-        
+
     def _step_callback(self):
         p.setJointMotorControlArray(
             bodyIndex=self.robot_id,
-            jointIndices=range(self.num_joints),
+            jointIndices=self.motor_indices,
             controlMode=p.TORQUE_CONTROL,
             forces=self.joint_states.effort,
         )
 
         p.stepSimulation()
 
-        for i in range(self.num_joints):
+        for i in range(len(self.motor_indices)):
             joint_state = p.getJointState(
-                bodyUniqueId=self.robot_id, jointIndex=i
+                bodyUniqueId=self.robot_id, jointIndex=self.motor_indices[i]
             )
             self.joint_states.position[i] = joint_state[0]
             self.joint_states.velocity[i] = joint_state[1]
